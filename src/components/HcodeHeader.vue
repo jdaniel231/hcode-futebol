6931a5b2973162fcdac6a6ff2428f02126bcc156
hammer200911 <jdaniel231@gmail.com>
2022-01-06 11:39:30 -0300

    novo projeoto e criar header

---
+++ src/components/HcodeHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <header>
+
+    <div class="container">
+      <nav class="navbar navbar-expand">
+        <a href="" class="link navbar-brand mr-auto">
+          <img src="./../assets/logo.svg" alt="Hcode Treinamento" class="img d-inline-block align-top" id="logo" > Futebol Clube
+        </a>
+        <div class="link mr-2">
+          <a href="" class="btn mr-5">Inicio</a>
+          <a href="" class="btn mr-5">Times</a>
+          <a href="" class="btn mr-5">Classificação</a>
+        </div>
+      </nav>
+    </div>
+
+  </header>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  header {
+    background-color: #000;
+
+  }
+  #logo {
+    width: 150px;
+    height: 100%;
+  }
+
+  .links{
+    display: inline-flex;
+  }
+
+  .link {
+    color: #d48e23;
+    font-size: 30px;
+    font-family: 'Rajdhani';
+  }
+
+  .link a{
+    color: #fff
+  }
+
+  .link a:focus{
+    outline: none !important;
+    box-shadow: none !important;
+  }
+
+</style>
+
