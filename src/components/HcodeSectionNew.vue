bf6400ac133ebb5e208b4e80f0603a1ab2692fbf
hammer200911 <jdaniel231@gmail.com>
2022-01-11 11:08:42 -0300

    getters

--- src/components/HcodeSectionNew.vue
+++ src/components/HcodeSectionNew.vue
@@ -19,6 +19,7 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import HcodeSectionNewsIndividual from './HcodeSectionNewsIndividual.vue'
 
 export default {
@@ -27,33 +28,17 @@ export default {
   },
   data() {
     return{
-      news: []
-    }
+      
+    }   
   },
-  created() {
-    this.news = [{
-      id: 1,
-      title: "Começa os treinos para a nova temporada",
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, voluptas aspernatur? At, deleniti et eos dicta ipsam iure animi nesciunt repellendus aliquam eligendi facilis mollitia alias eius quae, non facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci non ex nesciunt, vero necessitatibus eum laudantium voluptatibus? Aliquam quisquam possimus minus quis dolor quos nam totam mollitia tempora nihil',
-      date: '2021-01-05',
-      img: 'news1.jpg',
-      info: 'Noticia 1'
-    }, {
-      id: 2,
-      title: 'Jogo de quarta-feira',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, voluptas aspernatur? At, deleniti et eos dicta ipsam iure animi nesciunt repellendus aliquam eligendi facilis mollitia alias eius quae, non facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci non ex nesciunt, vero necessitatibus eum laudantium voluptatibus? Aliquam quisquam possimus minus quis dolor quos nam totam mollitia tempora nihil',
-      date: '2021-04-31',
-      img: 'news2.jpg',
-      info: 'Noticia 2'
-    }, {
-      id: 3,
-      title: 'A inaguração do estadio Castelão em Fortaleza-Ce',
-      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt, voluptas aspernatur? At, deleniti et eos dicta ipsam iure animi nesciunt repellendus aliquam eligendi facilis mollitia alias eius quae, non facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae adipisci non ex nesciunt, vero necessitatibus eum laudantium voluptatibus? Aliquam quisquam possimus minus quis dolor quos nam totam mollitia tempora nihil',
-      date: '2021-07-25',
-      img: 'news3.jpg',
-      info: 'Noticia 3'
-    }]
-  }
+
+  computed: {
+      ...mapGetters({
+        news: 'getNews'
+      })
+    }
+
+ 
 }
 </script>
 
